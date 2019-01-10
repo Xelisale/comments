e4655abf041031f37c13367fb7d05643ae468132
Alexandr <nvil05@rambler.ru>
2019-01-11 02:39:56 +0300

    Refactoring structure json answe

---
+++ app.py
@@ -0,0 +1,97 @@
+import json
+from flask import Flask, jsonify, abort
+from flask import make_response, request
+from werkzeug.contrib.fixers import ProxyFix
+
+
+from function import Search, WorksDB
+
+app = Flask(__name__)
+
+
+@app.route('/api/names', methods=['GET'])
+def get_all_name():
+	with open('names.json', 'r') as file:
+		names_all = json.load(file)
+
+	return jsonify({'names': names_all})
+
+
+@app.route('/api/names/find/', methods=['POST'])
+def names_find():
+	"""	Find and return all match in POST request.
+		Name in POST request necessary parameter
+	"""
+	if not (request.json and 'name' in request.json):
+		abort(400)
+
+	search = Search()
+	names = search.total(request.json['name'])
+
+	if names:
+		return jsonify({'names': names}), 201
+
+	else:
+		abort(404)
+
+
+@app.route('/api/names/find/id/', methods=['POST'])
+def one_comment():
+	""" :param requst.json['id']: id number vine
+		:return comment:   information user and comments
+	"""
+	result = []
+
+	if not (request.json and 'id' in request.json):
+		abort(400)
+
+	search = Search()
+	work_bd = WorksDB()
+	work_bd.create_database()
+	id = request.json['id']
+	data = work_bd.check_id(id)
+
+	if not ('cache' in request.json) and len(data) != 0:
+		return jsonify({'comments': data}), 201
+
+	else:
+		comments = json.loads(search.wine_comm(id))
+		result_all = comments['reviews']
+
+		for result_one in result_all:
+			id_comments = work_bd.check_id_comment(result_one['note'])
+
+			if id_comments:
+				id_comments = id_comments[0]
+			else:
+				work_bd.add_comment(result_one['note'], id)
+				id_comments = work_bd.return_numb()
+
+			comment = {
+				'author': result_one['user']['alias'],
+				'image': result_one['user']['image'],
+				'note': {'id': id_comments,
+						'text': result_one['note']}
+			}
+
+			result.append(comment)
+
+		bd_data = [id, str(result)]
+
+		if 'cache' in request.json:
+			work_bd.update_bd(bd_data)
+
+		else:
+			work_bd.insert(bd_data)
+
+	return jsonify({'comment': result}), 201
+
+
+@app.errorhandler(404)
+def not_found(error):
+	return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+app.wsgi_app = ProxyFix(app.wsgi_app)
+if __name__ == '__main__':
+	app.run()
---
+++ function.py
@@ -0,0 +1,173 @@
+import requests
+import json
+import re
+import sqlite3
+
+
+class Search:
+	"""Search a vine only in vivio"""
+	def __init__(self):
+		self.site = 'http://vivino.com/search/wines?q='
+
+	def page_result(self, ob_find):
+		""":return result: all page"""
+		data = requests.get(self.site + ob_find)
+		result = data.text.split('\n')
+		return result
+
+	@staticmethod
+	def convert(data):
+		""" :param data: Page after search
+			:return result: All coincidences
+		"""
+		for res in data:
+			if '"name":' in res:
+				result = json.loads(res)
+				return result
+
+	@staticmethod
+	def wine_id(url):
+		data_request = requests.get(url)
+		data = str(data_request.content)
+		result = re.findall('https://www.vivino.com/.+/w/(\d+)', data)
+		return result[0]
+
+	@staticmethod
+	def wine_comm(num):
+		data = requests.get('https://www.vivino.com/api/wines/' + num + '/reviews?per_page=10')
+		data = data.text
+		return data
+
+	def total(self, ob_find):
+		""":return names: dict  a) vine id(unique number)
+								b) vine name
+								c) vine image(url)
+		"""
+		names = []
+
+		data_page = self.page_result(ob_find)
+		data_json = self.convert(data_page)
+		if data_json:
+
+			for data in data_json:
+				print(data)
+				dat = self.wine_id(data['@id'])
+				result = [
+					dict(id=dat, name=data['name'], image=data['image'])
+				]
+				names.append(result)
+			return names
+
+
+class WorksDB:
+	"""class to work on DB"""
+
+	def __init__(self):
+		self.name_bd = 'Databases.sqlite'
+
+	def create_database(self):
+		con = sqlite3.connect(self.name_bd)
+		cursor = con.cursor()
+		sql = """
+			CREATE TABLE IF NOT EXISTS data (
+				numb INTEGER PRIMARY KEY AUTOINCREMENT, 
+				id_wine INTEGER unique, 
+				data_request TEXT, 
+				date_create TEXT
+				);
+		"""
+		sql2 = """
+			CREATE TABLE IF NOT EXISTS comments (
+			numb INTEGER PRIMARY KEY AUTOINCREMENT, 
+			wine_id INTEGER, 
+			comment_text TEXT, 
+			FOREIGN KEY(wine_id) REFERENCES data(id_wine))
+		"""
+		cursor.execute(sql)
+		con.commit()
+		cursor.execute(sql2)
+		con.commit()
+		con.close()
+
+	def add_comment(self, comment, wine_id):
+		con = sqlite3.connect(self.name_bd)
+		cursor = con.cursor()
+		sql = """
+		INSERT INTO comments(wine_id, comment_text) VALUES (?,?)
+		
+		"""
+		cursor.execute(sql, (wine_id, comment))
+		con.commit()
+		con.close()
+
+	def check_id_comment(self, text):
+		"""return data: new id or have id"""
+		con = sqlite3.connect(self.name_bd)
+		cursor = con.cursor()
+		sql = """
+			SELECT numb from comments where comment_text like ?
+		"""
+		cursor.execute(sql, (text,))
+		data = cursor.fetchone()
+		print("first data: ", data)
+		if data:
+			return data
+
+	def return_numb(self):
+		con = sqlite3.connect(self.name_bd)
+		cursor = con.cursor()
+		sql = """
+			SELECT numb FROM comments ORDER BY numb DESC LIMIT 1"
+		"""
+
+		try:
+			cursor.execute(sql)
+			data = cursor.fetchone()
+			data += 1
+		except sqlite3.OperationalError:
+			data = 1
+
+		return data
+
+	def check_id(self, id_wine):
+		con = sqlite3.connect(self.name_bd)
+		cursor = con.cursor()
+		sql = "SELECT id_wine, data_request FROM data WHERE id_wine =" + id_wine
+		cursor.execute(sql)
+		data = cursor.fetchone()
+		if data:
+			return data[1]
+		else:
+			return []
+
+	def insert(self, data_in):
+		con = sqlite3.connect(self.name_bd)
+		cursor = con.cursor()
+		sql = """
+					INSERT INTO data (id_wine, data_request, date_create) 
+					VALUES (?,?, current_date)
+				"""
+		try:
+			cursor.execute(sql, data_in)
+			con.commit()
+			con.close()
+		except sqlite3.IntegrityError:
+			print("This id is not unique")
+			pass
+
+	def update_bd(self, id_update):
+		""":param id_update [id_wine, data]"""
+		con = sqlite3.connect(self.name_bd)
+		cursor = con.cursor()
+		sql = """
+			UPDATE data 
+			SET data_request =?, date_create=CURRENT_DATE 
+			WHERE id_wine=?
+		 		"""
+		try:
+			cursor.execute(sql, (id_update[1], id_update[0]))
+			con.commit()
+			con.close()
+		except sqlite3.IntegrityError:
+			print("This id is not unique")
+			pass
